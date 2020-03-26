e26718718b6418ba20e7428fce1d74499483c574
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-03-26 09:54:36 -0500

    Update activate.bat

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -55,13 +55,13 @@ IF NOT "%CONDA_BUILD%" == "" (
 call :GetWin10SdkDir
 :: dir /ON here is sorting the list of folders, such that we use the latest one that we have
 for /F %%i in ('dir /ON /B "%WindowsSdkDir%\include"') DO (
-  SET testsdk=%%~i
-  echo itest1=%%i:~0,2%%
-  echo itest2=%i:~0,2%
-  echo itest3=%%%i:~0,2%%
-  echo first=%%testsdk%%
-  SET testsdk=%%testsdk:~0,2%%
-  echo second=%%testsdk%%  
+  set testsdk=%%~i
+  echo itest1="%%i:~0,2%%"
+  echo itest2="%i:~0,2%"
+  echo itest3="%%%i:~0,2%%"
+  echo first="%testsdk%"
+  set testsdk=%%testsdk:~0,2%%
+  echo second="%testsdk%"
   IF "%testsdk%" == "10" (
     SET WindowsSDKVer=%%~i
   )
