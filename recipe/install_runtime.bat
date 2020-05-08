619fce972b7fa482021c4fcdc27cc440d634741d
Isuru Fernando <isuruf@gmail.com>
2020-05-08 09:29:27 -0500

    Update dir command

--- recipe/install_runtime.bat
+++ recipe/install_runtime.bat
@@ -22,8 +22,7 @@ if not exist "%BT_ROOT%" (
     exit 1
 )
 
-cd "%BT_ROOT%\VC"
-dir /s /b
+dir "%BT_ROOT%\VC" /s /b
 
 set "REDIST_ROOT=%BT_ROOT%\VC\Tools\MSVC\%runtime_version%\%VC_PATH%"
 echo "%REDIST_ROOT%"
