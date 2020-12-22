f951e2e3458b936859be1cdada80fc9dcfca679a
Isuru Fernando <isuruf@gmail.com>
2020-12-21 21:31:13 -0600

    definitely the final typo fixed

--- recipe/vc_repack.py
+++ recipe/vc_repack.py
@@ -54,7 +54,7 @@ class AtTemplate(string.Template):
 
 def subs(line, args):
     t = AtTemplate(line)
-    vcvars_ver = "".join(args.version.split(".")[:2])
+    vcvars_ver = ".".join(args.version.split(".")[:2])
     return t.substitute(
          year=args.activate_year, ver=args.activate_major, target=args.arch,
          vcvars_ver=vcvars_ver, ver_plus_one=str(int(args.activate_major)+1),
