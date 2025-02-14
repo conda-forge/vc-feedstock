295be7fc8d16ebd3faadcf44aeb37a0913a943d6
H. Vetinari <h.vetinari@gmx.com>
2025-02-14 11:43:25 +1100

    add deactivation script

---
+++ recipe/deactivate.bat
@@ -0,0 +1,17 @@
+@@echo off
+setlocal enabledelayedexpansion
+
+:: restore variables (if they existed prior to activation, they have a value that's not "placeholder")
+for %%X in (
+    CC CXX CMAKE_ARGS CMAKE_GEN CMAKE_GENERATOR CMAKE_GENERATOR_PLATFORM CMAKE_GENERATOR_TOOLSET
+    CMAKE_PLAT CMAKE_PREFIX_PATH CONDA_BUILD_CROSS_COMPILATION DISTUTILS_USE_SDK INCLUDE LATEST_VS
+    LIB MSSdk MSYS2_ARG_CONV_EXCL MSYS2_ENV_CONV_EXCL NEWER_VS_WITH_OLDER_VC PY_VCRUNTIME_REDIST
+    USE_NEW_CMAKE_GEN_SYNTAX VCVARSBAT VSINSTALLDIR VS_MAJOR VS_VERSION VS_YEAR WindowsSDKVer
+) do (
+    if "!_CONDA_BACKUP_%%X!" == "placeholder" (
+        set "%%X="
+    ) else (
+        set "%%X=!_CONDA_BACKUP_%%X!"
+    )
+    set "_CONDA_BACKUP_%%X="
+)
