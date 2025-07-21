7fdb81278b2bfd231357855977e374c426301feb
H. Vetinari <h.vetinari@gmx.com>
2025-07-22 09:52:24 +1100

    update CMAKE_ARGS in one go in activate.bat

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,5 +1,4 @@
 @@echo on
-setlocal EnableDelayedExpansion
 
 :: Set env vars that tell distutils to use the compiler that we put on path
 SET DISTUTILS_USE_SDK=1
@@ -90,9 +89,8 @@ set "VCVARSBAT=@{vcvarsbat}"
 
 set "CMAKE_ARGS=-DCMAKE_BUILD_TYPE=Release"
 IF "%CONDA_BUILD%" == "1" (
-  set "CMAKE_ARGS=%CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PROGRAM_PATH=%BUILD_PREFIX%\bin;%BUILD_PREFIX%\Scripts;%BUILD_PREFIX%\Library\bin;%PREFIX%\bin;%PREFIX%\Scripts;%PREFIX%\Library\bin"
-  :: see https://github.com/conda-forge/conda-smithy/issues/2319
-  set "CMAKE_ARGS=!CMAKE_ARGS! -DPython_FIND_REGISTRY=NEVER -DPython3_FIND_REGISTRY=NEVER"
+  :: for -DPython_FIND_REGISTRY see https://github.com/conda-forge/conda-smithy/issues/2319
+  set "CMAKE_ARGS=%CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DPython_FIND_REGISTRY=NEVER -DPython3_FIND_REGISTRY=NEVER -DCMAKE_PROGRAM_PATH=%BUILD_PREFIX%\bin;%BUILD_PREFIX%\Scripts;%BUILD_PREFIX%\Library\bin;%PREFIX%\bin;%PREFIX%\Scripts;%PREFIX%\Library\bin"
 )
 
 IF NOT "@{target_platform}" == "@{host_platform}" (
